21c05312699b2b37362151cf26e5c0b998ca45a3
Prashant Patel <pacific1392@gmail.com>
2021-04-23 12:09:15 +0530

    fixed header, animated hamburger

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const hamburger = document.getElementsByClassName('hamburger')[0]
 const nav = document.getElementsByTagName('nav')[0]
 
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('close')
     nav.classList.toggle('show-menu')
 })
